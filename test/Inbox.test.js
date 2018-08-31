d595b02c809fd332e6048a10a1878499159d5009
Lei Xie <leixie@Theresas-MBP.architech.lan>
2018-08-31 14:39:07 -0400

    Init commit

---
+++ test/Inbox.test.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let Inbox;
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi! there!'] })
+        .send({ from: accounts[0], gas: '1000000' });
+
+    inbox.setProvider(provider)
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        assert.ok(inbox.options.address);
+    })
+});
